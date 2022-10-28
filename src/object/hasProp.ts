29d462f30526e1e8ae1d9b6798f56ddf4401c21c
Luke Davis <dowhileluke@gmail.com>
2022-10-28 16:04:21 -0700

    Update: hasProp accepts non-Record objects

--- src/object/hasProp.ts
+++ src/object/hasProp.ts
@@ -1,4 +1,5 @@
+/* eslint-disable @typescript-eslint/ban-types -- this is the specifially desired effect */
 /** A concise way to check hasOwnProperty */
-export function hasProp<T>(object: Record<string, T>, prop: string) {
+export function hasProp<T extends {}>(object: T, prop: string) {
 	return Object.prototype.hasOwnProperty.call(object, prop)
 }
