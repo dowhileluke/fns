f8021759ba7f3211def9cf32b83c00ce919e5846
Luke Davis <dowhileluke@gmail.com>
2022-12-02 12:22:19 -0800

    Docs: `ObjectFn` docstring

--- src/object/types.ts
+++ src/object/types.ts
@@ -1,2 +1,4 @@
 export type Entries<T extends Record<string, unknown>> = Array<[keyof T, T[keyof T]]>
+
+/** A callback signature used when iterating over objects. */
 export type ObjectFn<T extends Record<string, unknown>, U = unknown> = (value: T[keyof T], key: keyof T, object: T) => U
