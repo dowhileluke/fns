820150d5966951c483490615556a08a6b5abb8b0
Luke Davis <dowhileluke@gmail.com>
2023-05-31 01:23:13 -0700

    Docs: typo

--- src/object/hasProp.ts
+++ src/object/hasProp.ts
@@ -1,4 +1,4 @@
-/* eslint-disable @typescript-eslint/ban-types -- this is the specifially desired effect */
+/* eslint-disable @typescript-eslint/ban-types -- this is the specifically desired effect */
 /** Tests `hasOwnProperty` for the object. */
 export function hasProp<T extends {}, K extends PropertyKey>(object: T, prop: K): object is T & Record<K, unknown> {
 	return Object.prototype.hasOwnProperty.call(object, prop)
