96a212b6c6eaba9aed6438af1b552fba5091b2ae
Luke Davis <dowhileluke@gmail.com>
2023-06-01 20:51:13 -0700

    Fix: better typing for some truthy calls

--- src/array/truthy.test.ts
+++ src/array/truthy.test.ts
@@ -35,3 +35,25 @@ const zeroTest = falseFn() ? 1 : 0
 test('TypeScript inference', () => {
 	expect(truthy('pass', boolTest && 'fail', nullTest && 'fail', zeroTest && 'fail')).toEqual(['pass'])
 })
+
+test('args form should accept many types at once', () => {
+	// TS should use 3rd overload when it can't infer multiple types for 2nd overload
+	const result = truthy('str', null, falseFn(), 'str', 0)
+
+	expect(result).toEqual(['str', 'str'])
+})
+
+test('result array elements are a union of truthy types', () => {
+	const t = truthy([true, false, 'pass', ''])
+
+	// This breaks if truthy result is `true[] | "pass"[]` instead of `(true | "pass")[]`
+	const f = t.filter(x => typeof x === 'string')
+
+	expect(f).toHaveLength(1)
+})
+
+test('all falsy values -> never[]', () => {
+	const result = truthy(false, null, 0)
+
+	expect(result).toHaveLength(0)
+})
--- src/array/truthy.ts
+++ src/array/truthy.ts
@@ -5,13 +5,19 @@ function isArrayForm<T>(array: T[] | T[][]): array is T[][] {
 export type Falsy = false | null | undefined | 0 | ''
 
 /** Returns an array containing only truthy elements from the given array. */
-export function truthy<T>(array: Array<T | Falsy>): T[]
+export function truthy<T>(array: Array<T | Falsy>): Array<Exclude<T, Falsy>>
 /**
  * Returns an array containing only truthy values from the list of arguments.
  *
  * For example: `truthy(0, 1, 2)` -> `[1, 2]`
  */
-export function truthy<T>(...args: Array<T | Falsy>): T[]
+export function truthy<T>(...args: Array<T | Falsy>): Array<Exclude<T, Falsy>>
+/**
+ * Returns an array containing only truthy values from the list of arguments.
+ *
+ * For example: `truthy(0, 1, 2)` -> `[1, 2]`
+ */
+export function truthy<T extends any[]>(...args: T): Array<Exclude<T[number], Falsy>>
 export function truthy<T>(...args: Array<T | Falsy> | Array<Array<T | Falsy>>) {
 	const arrayToFilter = isArrayForm(args) ? args[0] : args
 
